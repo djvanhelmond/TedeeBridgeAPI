8a8c6fa8a9f4308af799b80999d1b7071b38a34f
djvanhelmond <djvanhelmond@gmail.com>
2023-11-28 14:36:59 +0100

    First version of the Tedee Local Bridge API Python Library

--- demo.py
+++ demo.py
@@ -5,10 +5,15 @@ from TedeeBridgeAPI import tedeeCommon
 
 hostname = "10.0.0.1"                                      # IP Address of the host running the callback server
 serverPort = 14353
-bridgeIP = "10.0.0.10"                                     # IP Address for the Tedee Bridge
-bridgeToken = "GT34RG3rTrwW"                               # Token for the Tedee Bridge
+bridgeIP_1 = "10.0.0.10"                                   # IP Address for the Tedee Bridge
+bridgeToken_1 = "GT34RG3rTrwW"                             # Token for the Tedee Bridge
+bridgeIP_2 = "10.0.0.11"                                   # IP Address for the Tedee Bridge
+bridgeToken_2 = "sD4fwEtyGFed"                             # Token for the Tedee Bridge
+
 tc = tedeeCommon(hostname ,serverPort, logging.DEBUG)      # ERROR < INFO < DEBUG
-tc.loadBridge(bridgeIP, bridgeToken)
+
+tc.loadBridge(bridgeIP_1, bridgeToken_1)
+tc.loadBridge(bridgeIP_2, bridgeToken_2)                    # You can load multiple bridges
 
 
 print(tc.listLocks())
